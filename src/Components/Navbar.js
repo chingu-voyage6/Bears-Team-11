3a64762e122c15e0a65edf7bc09c5d75afdf6515
probuse <nprobuse@gmail.com>
2018-07-04 16:48:42 +0300

    add responsive fixed top navbar with search form

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,26 +1,30 @@
 import React, { Component } from 'react';
+import SearchBar from './SearchBar';
 class Navbar extends Component{
     render(){
         return(
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-            <div className="container">
-              <a className="navbar-brand" href="#">yungu</a>
-              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-              </button>
-              <div className="collapse navbar-collapse" id="navbarResponsive">
-                <ul className="navbar-nav ml-auto">
-                  <li className="nav-item">
-                    <a className="nav-link" href="#">Saved Playlists
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="#">Recently Watched</a>
-                  </li>
-                </ul>
+          <div>
+            <nav class="navbar navbar-default"></nav> {/* to deal with top padding issue */}
+            <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" rel="home" href="#" title="yungu">yungu</a>
               </div>
+              <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav">
+                  <li><a href="#">Saved Playlists</a></li>
+                </ul>
+                <div class="col-sm-3 col-md-3 pull-right">
+                  <SearchBar />                  
+                </div>
             </div>
-          </nav>
+          </div>
+        </div>
         )
     }
 }
