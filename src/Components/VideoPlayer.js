3048f7524e3aa81f9f80e67aafed4cf4b8fac01e
probuse <nprobuse@gmail.com>
2018-07-09 17:16:12 +0300

    remove div with bootstrap class col-md-8

--- src/Components/VideoPlayer.js
+++ src/Components/VideoPlayer.js
@@ -3,10 +3,8 @@ import React, { Component } from 'react';
 class VideoPlayer extends Component{
     render(){
         return(
-            <div className="col-md-8">
-                <div class="embed-responsive embed-responsive-16by9">
-                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/kjBOesZCoqc" allowfullscreen></iframe>
-                </div>
+            <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/kjBOesZCoqc" allowfullscreen></iframe>
             </div>
         )
     }
